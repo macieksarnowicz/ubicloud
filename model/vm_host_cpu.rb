cf3ff7781c77d6065057343700c96c57b0f28906
Hadi Moshayedi <hadi@ubicloud.com>
2025-01-09 22:39:30 -0800

    Create VmHostCpu records when setting up a VmHost.

---
+++ model/vm_host_cpu.rb
@@ -0,0 +1,18 @@
+require_relative "../model"
+
+class VmHostCpu < Sequel::Model
+  many_to_one :vm_host
+end
+
+# Table: vm_host_cpu
+# Primary Key: (vm_host_id, cpu_number)
+# Columns:
+#  vm_host_id       | uuid    |
+#  cpu_number       | integer |
+#  spdk             | boolean | NOT NULL
+#  vm_host_slice_id | uuid    |
+# Indexes:
+#  vm_host_cpu_pkey | PRIMARY KEY btree (vm_host_id, cpu_number)
+# Foreign key constraints:
+#  vm_host_cpu_vm_host_id_fkey       | (vm_host_id) REFERENCES vm_host(id)
+#  vm_host_cpu_vm_host_slice_id_fkey | (vm_host_slice_id) REFERENCES vm_host_slice(id)
